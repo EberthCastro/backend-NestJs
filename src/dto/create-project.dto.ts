309cebd52f702decbf41b3851de84436398e5e1a
Eberth <itec_19@hotmail.com>
2023-08-12 15:04:27 +0200

    included class validator in create and update dto's

--- src/dto/create-project.dto.ts
+++ src/dto/create-project.dto.ts
@@ -1,7 +1,20 @@
-export interface CreateProjectDto {
+import { IsString, IsBoolean, IsOptional, IsNotEmpty } from "class-validator";
+export class CreateProjectDto {
+    @IsString() 
+    @IsNotEmpty()
     title: string;
+
+    @IsString()
+    @IsOptional()
     description?: string;
+
+    @IsString()
     image: string;
+
+    @IsString()
     category: string;
+
+    @IsBoolean()
+    @IsOptional()
     done?: boolean;
 }
--- src/dto/update-project.dto.ts
+++ src/dto/update-project.dto.ts
@@ -1,7 +1,23 @@
-export interface UpdateProjectDto {
+import { IsString, IsBoolean, IsOptional, IsNotEmpty } from "class-validator";
+
+export class UpdateProjectDto {
+    @IsString()
+    @IsOptional()
     title?: string;
+    
+    @IsString()
+    @IsOptional()
     description?: string;
+
+    @IsString()
+    @IsOptional()
     image?: string;
+
+    @IsString()
+    @IsOptional()
     category?: string;
+
+    @IsBoolean()
+    @IsOptional()
     done?: boolean;
 }
